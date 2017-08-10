8e4ad7bd14823ad189a01edd7e591e1417ca5297
Robert J. Berger <rberger@ibd.com>
2017-08-09 22:29:20 -0700

    Superflous commit to test taggs

--- src/semver_from_git/core.cljs
+++ src/semver_from_git/core.cljs
@@ -27,7 +27,7 @@
 ;; Write new-tag to version-file
 
 ;; The id of the "empty tree" in Git and it's always available in every repository.
-;; This could change if git ever stopped using sha1
+; This could change if git ever stopped using sha1
 ;; See https://stackoverflow.com/a/40884093/38841
 (def git-empty-tree "4b825dc642cb6eb9a060e54bf8d69288fbee4904")
 
@@ -64,7 +64,7 @@
 (defn latest-semver-tag
   "Get the latest (largest) semver of a git repo"
   []
-  (.stdout (sh "git tag --sort=v:refname |sed -n 's/^\\([0-9]*\\.[0-9]*\\.[0-9]*\\).*/\\1/p' | tail -1")))
+  (:stdout (sh "git tag --sort=v:refname |sed -n 's/^\\([0-9]*\\.[0-9]*\\.[0-9]*\\).*/\\1/p' | tail -1")))
 
 (defn latest-major-minor []
   (if-let [match (re-matches #"Release-(\d+)\.(\d+)" (:stdout (sh "git describe --tags --match RELEASE-\\*")))]
@@ -77,13 +77,16 @@
 
 (defn new-semver []
     (let [initial-latest-tag (if (empty? (latest-semver-tag)) git-empty-tree)
-          increment  (if (> 0 (git-distance initial-latest-tag)) 1 0)]
-      (if-let [match ((re-matches #"(\d+\.)(\d+)\.(\d+)" initial-latest-tag))]
-        (str (nth  match 1) "." (nth match 2) "." (+ increment (nth match 3)))
+          _ (println "initial-latest-tag: " initial-latest-tag)
+          increment  (if (> 0 (git-distance initial-latest-tag)) 1 0)
+          _ (println "increment: " increment)]
+
+      (if-let [match (re-matches #"(\d+\.)(\d+)\.(\d+)" initial-latest-tag)]
+        (println "match: " match " if-let true result: " (str (nth  match 1) "." (nth match 2) "." (+ increment (nth match 3))))
         "0.0.0")))
 
 (defn -main []
-  (new-semver)
+  (println "new-semver result: " (new-semver)))
 
 
 ;;  (.exec shell "ls"))
